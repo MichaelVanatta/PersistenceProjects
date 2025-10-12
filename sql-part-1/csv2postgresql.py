197d58c20cfc5b2ca46ec3727a768bb888e502c5
Nate <nwillett@student.neumont.edu>
2025-10-12 17:01:10 -0600

    changed casing of csv to match the case of the one in-repo

--- sql-part-1/csv2postgresql.py
+++ sql-part-1/csv2postgresql.py
@@ -1,7 +1,7 @@
 import psycopg2, os
 
 def comma_death(old_file:str):
-    with open(f"./CSV/{old_file}", 'r') as infile, open("CSV/temp.csv", 'w') as outfile:
+    with open(f"./csv/{old_file}", 'r') as infile, open("csv/temp.csv", 'w') as outfile:
         for line in infile:
             stripped_line = line.strip()
             if stripped_line.endswith(','):
@@ -10,14 +10,14 @@ def comma_death(old_file:str):
                 modified_line = stripped_line
             outfile.write(modified_line + '\n')
 
-    os.replace("CSV/temp.csv", f"./CSV/{old_file}")
+    os.replace("csv/temp.csv", f"./csv/{old_file}")
     print("Trailing commas removed")
 
 def parse_local_csv():
-    for file in os.listdir('./CSV'):
-        open("./CSV/temp.csv", 'x')
+    for file in os.listdir('./csv'):
+        open("./csv/temp.csv", 'x')
         comma_death(file)
-        with open(f"CSV/{file}", 'r') as f:
+        with open(f"csv/{file}", 'r') as f:
             next(f)
             if file.replace("nw.", "nw_").replace(".csv", "") == 'nw_data.1.AllData':
                 cur.copy_from(f, 'nw_all_data', sep=",")
